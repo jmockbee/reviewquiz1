2d97093724581f87ed2f8dc018fac03f057a0880
Jon Mockbee <mockbee1000@gmail.com>
2021-08-04 21:06:02 -0400

    changes

--- problems.py
+++ problems.py
@@ -3,3 +3,10 @@ tip = bill * 0.15
 total = bill + tip
 share = (total/5) 
 print("Each person needs to pay:" + str(share))
+
+
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
